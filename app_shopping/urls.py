100dd86a424dfccafb45e7324b0d61149b11359c
Mhd-Hsyn <syd.mhd.hsyn@gmail.com>
2023-09-15 22:03:29 +0500

    Admin forgetpassword , changedpassword, emailOTP, logout

--- app_shopping/urls.py
+++ app_shopping/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AdminViewset
+from .views import *
 
 # Create a router for the viewset
 router = DefaultRouter()
-router.register(r"admin", AdminViewset, basename="admin")
+router.register(r"adminauth", AdminAuthViewset, basename="adminauth")
+router.register(r"admin", AdminViewset, basename= "admin")
 
 urlpatterns = [
     # Include the router-generated URLs
